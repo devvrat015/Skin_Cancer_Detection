0a16510f0011cf5b216315339beccf257462e4dd
Devvrat Kokane <devvratkokane@gmail.com>
2026-02-04 23:09:34 +0530

    commited training phase 1 and 2

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -8,42 +8,28 @@ from PIL import Image
 class ISICSkinDataset(Dataset):
     def __init__(self, csv_file, image_dir, transform=None):
         """
-        csv_file   : processed CSV (train or val)
-        image_dir  : directory containing raw images
-        transform  : torchvision transforms
+        csv_file  : Path to processed CSV (train_binary.csv or val_binary.csv)
+        image_dir : Path to raw images directory
         """
-        nb_dir = Path.cwd()
-        project_root = nb_dir if (nb_dir / 'data').exists() else nb_dir.parent
-        csv_file = pd.read_csv(str(project_root / 'data' / 'processed' / 'train' / 'train_binary.csv'))
-        image_dir = Path(project_root / 'data' / 'raw' / 'train' / 'images_train')
-
-        self.df = csv_file
+        self.df = pd.read_csv(csv_file)
         self.image_dir = image_dir
         self.transform = transform
 
     def __len__(self):
-        # Total number of samples
         return len(self.df)
 
     def __getitem__(self, idx):
-        # Get one row from processed CSV
         row = self.df.iloc[idx]
 
-        # Image ID from CSV
         image_id = row["isic_id"]
-
-        # Label (already binary: 0 or 1)
         label = torch.tensor(row["label"], dtype=torch.long)
 
-        # Construct image path
-        image_path = f"{self.image_dir}/{image_id}.jpg"
-
-        # Load image
+        image_path = self.image_dir / f"{image_id}.jpg"
         image = Image.open(image_path).convert("RGB")
 
-        # Apply transforms (if any)
         if self.transform:
             image = self.transform(image)
 
         return image, label
+
 
